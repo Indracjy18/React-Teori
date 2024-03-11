6c2d6af768c10ee92a746ad459b000b8c00eacd7
unknown <indracahyajaty@gmail.com>
2024-03-11 20:44:43 +0700

    Event Handler React

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,15 @@
 const Intro = (props) => {
+  const handleClick = (nama) => {
+    console.log("click" + nama);
+  };
+
   return (
     <div>
       <p>nama saya {props.nama}</p>
       <p>hobby saya {props.hobby}</p>
       <p>umur saya {props.umur}</p>
+      {/* cara ngeparse value ke handle click*/}
+      <button onClick={() => handleClick(props.nama)}>CLICK ME</button>
     </div>
   );
 };
